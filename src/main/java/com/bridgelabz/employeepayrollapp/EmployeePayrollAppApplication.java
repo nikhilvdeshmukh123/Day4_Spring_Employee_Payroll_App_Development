3a47623094723771a500c42ee0f12878b456b495
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-08-23 23:29:14 +0530

    Added: application.dev and application.prod environment to Application

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		System.out.println("Welcome to Employee Payroll Application");
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll Application Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        System.out.println("Welcome to Employee Payroll Application");
+    }
 }
